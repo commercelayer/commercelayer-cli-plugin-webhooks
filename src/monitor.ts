bc6eaa2623456dbbc0b4617127ccfcbf69e77a25
Pierluigi Viti <pierluigi@commercelayer.io>
2021-09-22 17:25:46 +0200

    fix progress monitor visualization

--- src/monitor.ts
+++ src/monitor.ts
@@ -7,10 +7,25 @@ import { center } from './common'
 
 const MAX_IMPORT_SIZE = apiConf.imports_max_size
 
-const MONITOR_BAR_SIZE = 30
+const TERMINAL_SIZE = process.stdout.columns || 80
+
+export { TERMINAL_SIZE }
 
 
-type HeaderColumn = { title: string; width: number; pad: boolean; style: Function }
+type MonitorStyle = {
+	format: string;
+	header: string;
+	colors: boolean;
+}
+
+type HeaderColumn = {
+	title: string;
+	width: number;
+	pad?: boolean;
+	style?: Function;
+	valueStyle?: Function;
+	hiddenHeader?: boolean;
+}
 
 type Payload = {
 	importId?: string;
@@ -21,33 +36,48 @@ type Payload = {
 	status?: string;
 }
 
-
 class Monitor {
 
 	private multibar: MultiBar
 
 	private totalItems: number
 
-	constructor(totalItems: number) {
+	private style: MonitorStyle
+
+
+	constructor(totalItems: number, colors?: boolean) {
 		this.totalItems = totalItems
+		this.style = this.monitorStyle(colors || true)
 		this.multibar = new cliProgress.MultiBar({
-			format: `| ${chalk.blueBright('{import}')} | {range} | ${chalk.greenBright('{bar}')} | ${chalk.yellowBright('{percentage}%')} | {status} | {tbp} | {processed} | {warnings} | {errors} | ${chalk.italic('{message}')}`,
+			format: this.style.format,
 			barCompleteChar: '\u2588',
 			barIncompleteChar: '\u2591',
 			hideCursor: true,
 			clearOnComplete: false,
 			autopadding: true,
 			formatValue: barFormatValue,
-			barsize: MONITOR_BAR_SIZE,
+			barsize: Monitor.BAR_SIZE,
 		})
 	}
+
+	static readonly BAR_SIZE = 25
+
+	static readonly FORMATS: Array<string> = [
+		'| {import} | {range} | {bar} | {percentage}% | {status} | {tbp} | {processed} | {warnings} | {errors} | {message}',
+		'| {import} | {range} | {bar} | {percentage}% | {status} | {tbp} | {processed} | {warnings} | {errors} |',
+		'| {import} | {range} | {percentage}% | {status} | {tbp} | {processed} | {warnings} | {errors} | {message}',
+		'| {import} | {range} | {percentage}% | {status} | {tbp} | {processed} | {warnings} | {errors} |',
+		'| {import} | {range} | {percentage}% | {status} | {tbp} | {message}',
+		'| {import} | {range} | {percentage}% | {status} | {tbp} |',
+		'| {import} | {range} | {percentage}% | {status} |',
+	]
 
 
 	static create(totalItems: number, log?: Function) {
 		const monitor = new Monitor(totalItems)
 		if (log) {
 			log()
-			log(monitor.progressHeader())
+			log(monitor.style.header)
 		}
 		return monitor
 	}
@@ -87,7 +117,7 @@ class Monitor {
 		if (payload?.errors) updPayload.errors = String(payload.errors).padStart(maxImportLength, ' ')
 		if (payload?.importId) updPayload.import = payload.importId
 
-		if (payload?.status) updPayload.status = statusStyle(payload.status, payload.processed)
+		if (payload?.status) updPayload.status = this.statusStyle(payload.status, payload.processed)
 
 		updPayload.tbp = String(bar.getTotal() - updCount).padStart(maxImportLength, ' ')
 
@@ -101,6 +131,7 @@ class Monitor {
 		if (!message) return ''
 
 		const msg = message.trim()
+		if (!this.style.colors) return msg
 
 		if (style) switch (style.toLowerCase()) {
 			case 'w':
@@ -109,48 +140,140 @@ class Monitor {
 			case 'e':
 			case 'err':
 			case 'error': return chalk.redBright(msg)
-			default: return ' '
+			default: return ''
 		}
 		else return msg
 
 	}
 
 
-	private progressHeader() {
+	private availableColumns(): { [key: string]: HeaderColumn } {
 
 		const itemsLength = String(this.totalItems).length
 		const maxImportsLength = String(MAX_IMPORT_SIZE).length
 
-		const header: any = [
-			{ title: 'ID', width: 10, pad: true },
-			{ title: 'Items', width: (itemsLength * 2) + 1, pad: true },
-			{ title: 'Import progress', width: MONITOR_BAR_SIZE, pad: true },
-			{ title: ' %', width: 4, pad: true },
-			{ title: ' Status', width: 11, pad: true },
-			{ title: 'TBP\u2193', width: maxImportsLength, pad: true, style: chalk.cyanBright },
-			{ title: 'Prc.', width: maxImportsLength, pad: true, style: chalk.greenBright },
-			{ title: 'Wrn.', width: maxImportsLength, pad: true, style: chalk.yellowBright },
-			{ title: 'Err.', width: maxImportsLength, pad: true, style: chalk.redBright },
-		]
+		const columns: { [key: string]: HeaderColumn } = {
+			import: { title: 'ID', width: 10, pad: true, valueStyle: chalk.blueBright },
+			range: { title: 'Items', width: (itemsLength * 2) + 1, pad: true },
+			bar: { title: 'Import progress', width: Monitor.BAR_SIZE, pad: true, valueStyle: chalk.greenBright },
+			percentage: { title: ' %', width: 4, pad: true, valueStyle: chalk.yellowBright },
+			status: { title: ' Status', width: 11, pad: true },
+			tbp: { title: 'TBP\u2193', width: maxImportsLength, pad: true, style: chalk.cyanBright },
+			processed: { title: 'Prc.', width: maxImportsLength, pad: true, style: chalk.greenBright },
+			warnings: { title: 'Wrn.', width: maxImportsLength, pad: true, style: chalk.yellowBright },
+			errors: { title: 'Err.', width: maxImportsLength, pad: true, style: chalk.redBright },
+			message: { title: '', hiddenHeader: true, width: 'Error'.length + 1, valueStyle: chalk.redBright },
+		}
 
+		return columns
+
+	}
+
+
+	private selectMonitorFormat(colors?: boolean): { columns: HeaderColumn[]; format: string } | undefined {
+
+		const allColumns = this.availableColumns()
+
+		for (const f of Monitor.FORMATS) {
+
+			// Extract column name from format
+			const cols: Array<string> = []
+			f.split('|').forEach(p => {
+				const col = p.trim().replaceAll(/[^a-zA-Z_]/g, '').trim()
+				if (col) cols.push(col)
+			})
+
+			// Calculate maximum row size
+			let totalSize = cols.length
+			cols.forEach(c => {
+				const col = allColumns[c]
+				let size = col.width || 0
+				if (c === 'percentage') size++
+				if ((colors && c === 'status')) size += 10
+				if (col.pad) size += 2
+				if (colors && col.valueStyle) size += 10
+				totalSize += size
+			})
+
+			// If total size fits console width select the format
+			if (totalSize <= (process.stderr.columns || 80)) {
+				let format = f
+				if (colors) {
+					cols.forEach(c => {
+						const style = allColumns[c].valueStyle
+						const perc = (c === 'percentage') ? '%' : ''
+						if (style) format = format.replace(new RegExp(`{${c}}${perc}`), style(`{${c}}${perc}`))
+					})
+				}
+				return {
+					format,
+					columns: cols.map(c => allColumns[c]).filter(c => !c.hiddenHeader),
+				}
+			}
+
+		}
+
+	}
+
+
+	private monitorStyle(colors: boolean): MonitorStyle {
+
+		const style: MonitorStyle = {
+			header: '',
+			format: '',
+			colors,
+		}
+
+		// Select a monitor format that fit the current terminal size
+		let monitorFormat = this.selectMonitorFormat(style.colors)
+		if (!monitorFormat && style.colors) monitorFormat = this.selectMonitorFormat(style.colors = false)
+		if (!monitorFormat) throw new Error('No available monitor style for this terminal size')
+
+		style.format = monitorFormat.format
+
+		// Apply style to header columns
+		const header = monitorFormat.columns
 		const labels = header.map((h: HeaderColumn) => {
 
 			const w = (h.width + (h.pad ? 2 : 0))
 			const label = center(h.title, w)
 
 			let styled = chalk.bold(label)
-			if (h.style) styled = h.style(styled)
+			if (style.colors && h.style) styled = h.style(styled)
 
 			return styled
 
 		})
 
+		// Write header lower border
 		let tableWidth = header.length + 1
-		header.forEach((h: any) => {
+		header.forEach(h => {
 			tableWidth += h.width + (h.pad ? 2 : 0)
 		})
 
-		return `|${labels.join('|')}|\n` + ''.padStart(tableWidth, '-')
+		style.header = `|${labels.join('|')}|\n` + ''.padStart(tableWidth, '-')
+
+
+		return style
+
+	}
+
+
+	private statusStyle(status: string, processed?: number): string {
+
+		let s = status
+		if ((s.includes('in_progress')) && (processed === 0)) s = 'waiting ...'
+		s = s.padEnd(11, ' ')
+
+		if (this.style.colors) {
+			if (s.includes('completed')) s = chalk.greenBright(s)
+			else
+			if (s.includes('waiting')) s = chalk.italic(s)
+			else
+			if (s.includes('interrupted')) s = chalk.redBright(s)
+		}
+
+		return s
 
 	}
 
@@ -179,22 +302,6 @@ const barFormatValue = (v: any, _options: any, type: string) => {
 
 }
 
-
-const statusStyle = (s: string, processed?: number): string => {
-
-	let status = s
-	if ((status.includes('in_progress')) && (processed === 0)) status = 'waiting ...'
-	status = status.padEnd(11, ' ')
-
-	if (status.includes('completed')) status = chalk.greenBright(status)
-	else
-	if (status.includes('waiting')) status = chalk.italic(status)
-	else
-	if (status.includes('interrupted')) status = chalk.redBright(status)
-
-	return status
-
-}
 
 
 export { Monitor }
